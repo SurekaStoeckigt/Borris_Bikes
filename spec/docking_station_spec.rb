0eda829d0fb4a3e55234cce33597ea237632cead
Student <student@Admins-MacBook-Pro-3.local>
2019-02-07 09:11:40 +0000

    rspec one-liner for release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,5 @@
 require 'docking_station'
+
 describe DockingStation do
-  it 'responds to release_bike' do
-  docking_station = DockingStation.new
-  expect(docking_station).to respond_to :release_bike
-  #expect(subject).to respond_to :release_bike
-  end
+it {is_expected.to respond_to :release_bike }
 end
